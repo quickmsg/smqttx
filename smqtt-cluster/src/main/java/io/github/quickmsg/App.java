18db49645c9ddf9a2315dae0c79049d29d91f16e
luxurong <luxurong520@hellobike.com>
2021-10-23 23:14:38 +0800

    cluster

---
+++ smqtt-cluster/src/main/java/io/github/quickmsg/App.java
@@ -0,0 +1,13 @@
+package io.github.quickmsg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
