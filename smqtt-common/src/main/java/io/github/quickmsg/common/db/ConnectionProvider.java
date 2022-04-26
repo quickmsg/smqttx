4ead7b85a44f9a457aa979661d76168e9d2b5f5a
Easy <crazybeginner@qq.com>
2022-04-26 20:00:36 +0800

    sql auth

---
+++ smqtt-common/src/main/java/io/github/quickmsg/common/db/ConnectionProvider.java
@@ -0,0 +1,37 @@
+package io.github.quickmsg.common.db;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+
+/**
+ * 数据库连接提供者
+ *
+ * @author easy
+ * @date 2022/04/26
+ */
+public interface ConnectionProvider {
+
+
+    /**
+     * 初始化
+     *
+     * @param properties 配置
+     */
+    void init(Properties properties);
+
+    /**
+     * 获取链接
+     *
+     * @return {@link Connection}
+     */
+    Connection getConnection();
+
+
+    /**
+     * 关闭链接
+     */
+    void shutdown();
+
+
+}
