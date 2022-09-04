a3135f45aef7321763a0c87174dfa04ca72b01bd
luxurong <1074455781@qq.com>
2022-09-04 23:10:03 +0800

    調整結構

--- smqttx-common/src/main/java/io/github/quickmsg/common/integrate/cache/ConnectCache.java
+++ smqttx-common/src/main/java/io/github/quickmsg/common/integrate/cache/ConnectCache.java
@@ -1,9 +1,9 @@
 package io.github.quickmsg.common.integrate.cache;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.github.quickmsg.common.channel.MqttChannel;
 import io.netty.handler.codec.mqtt.MqttVersion;
 import lombok.Data;
+import org.apache.ignite.cache.query.annotations.QuerySqlField;
 
 /**
  * @author luxurong
@@ -12,21 +12,30 @@ import lombok.Data;
 @Data
 public class ConnectCache {
 
+    @QuerySqlField(index = true)
     private String clientId;
 
+    @QuerySqlField
     private String clientAddress;
 
+    @QuerySqlField
     private String nodeIp;
 
+    @QuerySqlField
     private MqttVersion version;
 
+    @QuerySqlField
     private int keepalive;
 
+    @QuerySqlField
     private boolean cleanSession;
 
+    @QuerySqlField( descending = true)
     private String connectTime;
 
+    @QuerySqlField
     private MqttChannel.Auth auth;
 
+    @QuerySqlField
     private MqttChannel.Will will;
 }
