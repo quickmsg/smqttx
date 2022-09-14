242812eb6d3ca7e482ca9c877e75b6bdeb20a85b
luxurong <1074455781@qq.com>
2022-09-14 23:26:42 +0800

    修复推送数据bug

--- smqttx-common/src/main/java/io/github/quickmsg/common/message/mqtt/ClusterMessage.java
+++ smqttx-common/src/main/java/io/github/quickmsg/common/message/mqtt/ClusterMessage.java
@@ -6,6 +6,8 @@ import io.github.quickmsg.common.context.ReceiveContext;
 import io.github.quickmsg.common.message.Message;
 import lombok.Data;
 
+import java.util.Optional;
+
 /**
  * @author luxurong
  */
@@ -38,8 +40,8 @@ public class ClusterMessage{
         this.retain = message.isRetain();
         this.body = message.getBody();
         this.connectTime = message.getTime();
-        this.channelId= message.getMqttChannel().getId();
-        this.clientId= message.getMqttChannel().getClientId();
+        this.channelId= Optional.ofNullable(message.getMqttChannel()).map(MqttChannel::getId).orElse(0);
+        this.clientId= message.getClientId();
     }
 
     public PublishMessage toPublishMessage() {
